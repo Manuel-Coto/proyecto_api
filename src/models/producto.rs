35f39afd79dc36d1d3f6ca652db7079c0cd8f7f2
jonathan rivera <rl22034@ues.edu.sv>
2026-05-21 12:27:38 -0600

    - Se crearon los modelos Producto, NuevoProducto y ActualizarProducto con soporte para serde y sqlx (FromRow). - Se implemento ProductoRepository con el patrón CRUD completo (obtener, crear, actualizar, eliminar).

---
+++ src/models/producto.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow, PartialEq)]
+pub struct Producto{
+    pub id: i32,
+    pub nombre: String,
+    pub precio: f64,
+    pub stock: i32,
+    #[sqlx(rename ="id_categoria")]
+    pub categoria: i32,
+    #[sqlx(rename = "id_proveedor")]
+    pub proveedor: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NuevoProducto{
+    pub nombre: String,
+    pub precio: f64,
+    pub stock: i32,
+    pub categoria: i32,
+    pub proveedor: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ActualizarProducto {
+    pub nombre: Option<String>,
+    pub precio: Option<f64>,
+    pub stock: Option<i32>,
+    pub categoria: Option<i32>,
+    pub proveedor: Option<i32>,
+}
+
---
+++ src/repository/producto_repository.rs
@@ -0,0 +1,71 @@
+use sqlx::{PgPool, Row};
+use crate::models::producto::{Producto, NuevoProducto, ActualizarProducto};
+
+pub struct ProductoRepository{
+    pool: PgPool,
+}
+
+impl ProductoRepository{
+    pub fn new(pool: PgPool) -> Self {
+        Self{pool}
+    }
+
+    pub async fn obtener_producto(&self) -> sqlx::Result<Vec<Producto>>{
+        let productos = sqlx::query_as::<_, Producto>(
+            "SELECT id, nombre, precio, stock, categoria, proveedor
+                 FROM producto").fetch_all(&self.pool).await?;
+        Ok(productos)
+    }
+
+    pub async fn crear_producto(&self, nuevo: NuevoProducto) -> sqlx::Result<Producto>{
+        let producto = sqlx::query_as::<_, Producto>(
+            r#"
+                   INSERT INTO Productos (nombre, precio, stock, id_categoria, id_proveedor)
+                   VALUES ($1, $2, $3, $4, $5)
+                   RETURNING id_producto, nombre, precio, stock, id_categoria, id_proveedor"#
+        )
+            .bind(nuevo.nombre)
+            .bind(nuevo.precio)
+            .bind(nuevo.stock)
+            .bind(nuevo.categoria)
+            .bind(nuevo.proveedor)
+            .fetch_one(&self.pool)
+            .await?;
+        Ok(producto)
+    }
+
+    pub async fn actualizar_producto(&self, id: i32, cambios: ActualizarProducto) -> sqlx::Result<Producto> {
+        let producto = sqlx::query_as::<_, Producto>(
+            r#"
+            UPDATE Productos
+            SET
+                nombre = COALESCE($1, nombre),
+                precio = COALESCE($2, precio),
+                stock = COALESCE($3, stock),
+                id_categoria = COALESCE($4, id_categoria),
+                id_proveedor = COALESCE($5, id_proveedor)
+            WHERE id_producto = $6
+            RETURNING id_producto, nombre, precio, stock, id_categoria, id_proveedor
+            "#
+        )
+            .bind(cambios.nombre)
+            .bind(cambios.precio)
+            .bind(cambios.stock)
+            .bind(cambios.categoria)
+            .bind(cambios.proveedor)
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await?;
+
+        Ok(producto)
+    }
+
+    pub async fn eliminar_producto(&self, id: i32) -> sqlx::Result<()> {
+        sqlx::query("DELETE FROM Productos WHERE id_producto = $1")
+            .bind(id)
+            .execute(&self.pool)
+            .await?;
+
+        Ok(())
+    }
+}
