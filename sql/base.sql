9516931f0be8571625cd18b4a05416c1647324bb
Manuel-Coto <cm22045@ues.edu.sv>
2026-05-25 16:29:11 -0600

    Creando categoria en controller

---
+++ sql/base.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Categorias (
+    id_categoria SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion TEXT
+);
+
+CREATE TABLE Proveedores (
+    id_proveedor SERIAL PRIMARY KEY,
+    nombre_empresa VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20),
+    contacto VARCHAR(100)
+);
+
+CREATE TABLE Productos (
+    id_producto SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    stock INT DEFAULT 0,
+    id_categoria INT REFERENCES Categorias(id_categoria),
+    id_proveedor INT REFERENCES Proveedores(id_proveedor)
+);
+
+CREATE TABLE Clientes (
+    id_cliente SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    correo_electronico VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE Ventas (
+    id_venta SERIAL PRIMARY KEY,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_cliente INT REFERENCES Clientes(id_cliente),
+    id_producto INT REFERENCES Productos(id_producto),
+    cantidad INT NOT NULL
+);
--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,1 +1,2 @@
 pub mod producto_controller;
+pub mod categoria_controller;
